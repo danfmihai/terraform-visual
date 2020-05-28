d5689676e6ca666014bbc34f099334191fc9af4f
Mihai <danfmihai@hotmail.com>
2020-05-28 11:08:34 -0400

    subnet added

--- subnet.tf
+++ subnet.tf
@@ -1,8 +1,15 @@
-resource "aws_subnet" "dev" {
-  vpc_id     = aws_vpc.id
+resource "aws_subnet" "dev1" {
+  vpc_id     = aws_vpc.dev.id
   cidr_block = "10.0.1.0/24"
   tags = {
     Name = var.Name
   }
+}
 
+resource "aws_subnet" "dev1" {
+  vpc_id     = aws_vpc.dev.id
+  cidr_block = "10.0.2.0/24"
+  tags = {
+    Name = var.Name
+  }
 }
