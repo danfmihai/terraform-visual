d657862b91d7f90e3581d377e72a6ee21ddfeca7
Mihai <danfmihai@hotmail.com>
2020-05-28 11:25:06 -0400

    edited

--- asg.tf
+++ asg.tf
@@ -4,4 +4,5 @@ resource "aws_autoscaling_group" "dev" {
   min_size             = var.min_size
   max_size             = var.max_size
   availability_zones   = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  vpc_zone_identifier = [aws_subnet.dev1.id,aws_subnet.dev2.id]
 }
